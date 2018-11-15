003c76000521faba6514ea5cbd912a08ae5a2668
Es <eugns.p@gmail.com>
2018-11-15 12:34:51 +0300

    Update

--- include/es_fe/dof/tools.hpp
+++ include/es_fe/dof/tools.hpp
@@ -22,4 +22,15 @@ void compute_and_set_sparsity_pattern(const System& system, Coupling_fn&& coupli
 		system.template sparsity_pattern<Symmetry>(std::forward<Coupling_fn>(coupling_fn));
 	matrix.set_sparsity_pattern(std::move(pattern));
 }
+
+template<class Sparse_matrix, class System, class Coupling_fn>
+void compute_and_set_sparsity_pattern2(const System& system, Coupling_fn&& coupling_fn,
+									  Sparse_matrix& matrix)
+{
+	using Symmetry = typename Sparse_matrix::Symmetry_tag;
+
+	auto pattern =
+		system.template sparsity_pattern2<Symmetry>(std::forward<Coupling_fn>(coupling_fn));
+	matrix.set_sparsity_pattern(std::move(pattern));
+}
 } // namespace es_fe
