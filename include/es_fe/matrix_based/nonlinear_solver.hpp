ad7ff79a0aebd32a54287e1f3c3ba6cea61dad23
eugnsp <evgeny.sg@gmail.com>
2019-06-26 22:29:54 +0300

    Nonlinear solver

--- include/es_fe/matrix_based/nonlinear_solver.hpp
+++ include/es_fe/matrix_based/nonlinear_solver.hpp
@@ -8,15 +8,14 @@
 
 namespace es_fe
 {
-template<class System_, class Linear_solver_>
-class Matrix_based_nonlinear_solver : public Matrix_based_solver_base<System_, Linear_solver_>
+template<class System, class Linear_solver>
+class Matrix_based_nonlinear_solver : public Matrix_based_solver_base<System, Linear_solver>
 {
 private:
-	using Base = Matrix_based_solver_base<System_, Linear_solver_>;
+	using Base = Matrix_based_solver_base<System, Linear_solver>;
 
 public:
 	using Base::Base;
-
 	using Base::system;
 
 	template<class... Args>
@@ -33,22 +32,39 @@ public:
 		set_bnd_values();
 		linear_solver_.analyze();
 
+		double residual_norm_old = 0;
+		const double armijo = 1e-4;
+		double reduction_factor = 1;
+
 		unsigned int n_iter = 0;
 		while (true)
 		{
 			assemble();
 			const double residual_norm = rhs_norm2();
 
-			linear_solver_.factorize_solve(rhs_, step_);
-			solution_.rows_view(0, step_.size()) += step_;
+			if (n_iter > 0 && residual_norm > residual_norm_old * (1 - armijo * reduction_factor))
+			{
+				reduction_factor /= 2;
+				step_ /= 2;
+				solution_.rows_view(0, step_.size()) -= step_;
 
-			const auto step_norm = es_la::norm2(step_);
-			const auto solution_norm = es_la::norm2(solution_);
+				after_line_search_step(n_iter, residual_norm, reduction_factor);
+			}
+			else
+			{
+				reduction_factor = 1;
+				residual_norm_old = residual_norm;
 
-			after_step(n_iter, residual_norm, step_norm, solution_norm);
+				linear_solver_.factorize_solve(rhs_, step_);
+				solution_.rows_view(0, step_.size()) += step_;
 
-			if (step_norm / solution_norm < 1e-12 || n_iter > 50)
-				break;
+				const auto step_norm = es_la::norm2(step_);
+				const auto solution_norm = es_la::norm2(solution_);
+				after_step(n_iter, residual_norm, step_norm, solution_norm);
+
+				if (step_norm / solution_norm < 1e-12 || n_iter > 50)
+					break;
+			}
 
 			++n_iter;
 		}
@@ -79,6 +95,10 @@ protected:
 
 	virtual void after_step(
 		unsigned int /* n_iter */, double /* residual_norm */, double /* step_norm */, double /* solution_norm */)
+	{}
+
+	virtual void after_line_search_step(
+		unsigned int /* n_iter */, double /* residual_norm */, double /* reduction factor */)
 	{}
 
 protected:
